9db0a9181d082f64c4c54f22017abbce3ec244c2
PrishaTuli <prishatuli007@gmail.com>
2025-07-10 11:45:13 +0530

    listing.js

--- models/listing.js
+++ models/listing.js
@@ -30,7 +30,4 @@ listingSchema.post("findOneAndDelete",async(listing)=>{
 });
 const Listing=mongoose.model("Listing",listingSchema);
 module.exports=Listing;
-// image:{
-//     type:String,
-//     default:"https://images.unsplash.com/photo-1603477849227-705c424d1d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvcGljYWwlMjBiZWFjaHxlbnwwfHwwfHx8MA%3D%3D",
-//     set:(v)=>v==""?"https://images.unsplash.com/photo-1603477849227-705c424d1d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvcGljYWwlMjBiZWFjaHxlbnwwfHwwfHx8MA%3D%3D":v,},
+
